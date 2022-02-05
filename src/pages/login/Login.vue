a1c76fcaf79250d8d346bb07c85ae9ba5fc05e25
xmasuhai <xmasuhai@163.com>
2022-02-05 16:23:30 +0800

    ➕ unplugin-vue-components/vite ✨ 添加自动引入组件功能

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  Login
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Login'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,37 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+// 自动引入组件
+import autoComponents from 'unplugin-vue-components/vite';
+import {
+  ElementPlusResolver,
+  AntDesignVueResolver,
+  VantResolver,
+  HeadlessUiResolver,
+} from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    autoComponents({
+      // ui库解析器，也可以自定义
+      resolvers: [
+        ElementPlusResolver(),
+        AntDesignVueResolver(),
+        VantResolver(),
+        HeadlessUiResolver(),
+      ],
+      // 指定组件位置，默认是src/components
+      dirs: ['src/components'],
+      // ui库解析器
+      // resolvers: [ElementPlusResolver()],
+      extensions: ['vue'],
+      // 配置文件生成位置
+      dts: 'src/components.d.ts'
+    })
+  ],
   server: {
     host: '0.0.0.0' // 解决  Network: use --host to expose
   },
