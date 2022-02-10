0c65ca817e708d748779e5f4e7bf0307a665ec14
xmasuhai <xmasuhai@163.com>
2022-02-11 07:31:45 +0800

    🚧 ✨ 完善 BlogHeader 结构和样式

--- src/components/BlogHeader.tsx
+++ src/components/BlogHeader.tsx
@@ -1,6 +1,8 @@
-import {defineComponent} from 'vue';
-import {Button, } from 'ant-design-vue';
+import {computed, defineComponent, ref} from 'vue';
+import {Button,} from 'ant-design-vue';
+
 // CSS module
+import classNames from 'classnames';
 import blogClass from '@/styles/blog.module.scss';
 import basic from '@/styles/basic.module.scss';
 
@@ -13,19 +15,44 @@ export default defineComponent({
   props: BlogHeaderProps,
   components: {},
   setup(/*props, ctx*/) {
-    return {
+    const isLogin = ref(false);
 
-    }
+    const isLoginClass = computed(() => {
+      console.log(blogClass.login);
+      return (
+        isLogin
+          ? [blogClass.blogHeader, blogClass.login]
+          : [blogClass.blogHeader]);
+    });
+    return {
+      isLogin,
+      isLoginClass
+    };
   },
   render() {
+    const renderBtn = (btnString: string) => (<Button class={basic.blogBtn}>{btnString}</Button>);
+    const renderUnLogin = () => (
+      <div class={blogClass.btns}>
+        {renderBtn('立即登录')}
+        {renderBtn('注册账号')}
+      </div>
+    );
+
+    const renderLogin = () => (
+      <>
+        <i>123</i>
+        <img src="" alt=""/>
+      </>
+    );
+
     return (
-      <header class={blogClass.blogHeader}>
+      <header class={classNames(...this.isLoginClass)}>
         <h1>Let's share</h1>
         <p>精品博客汇聚</p>
-        <div>
-          <Button class={basic.blogBtn}>立即登录</Button>
-          <Button class={basic.blogBtn}>注册账号</Button>
-        </div>
+        {this.isLogin
+          ? renderLogin()
+          : renderUnLogin()
+        }
       </header>
     );
   }
