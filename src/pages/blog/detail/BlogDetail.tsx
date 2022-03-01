4baed4ecb420091bc4a10ab8e4ececbc27fdb4a7
xmasuhai <xmasuhai@163.com>
2022-03-01 16:46:48 +0800

    ♻️ ✏️ 重构 BlogDetail组件

--- src/pages/blog/detail/BlogDetail.tsx
+++ src/pages/blog/detail/BlogDetail.tsx
@@ -12,10 +12,26 @@ export default defineComponent({
 /*
     const BlogStore = useBlogStore();
 
+    const blogId = ref('')
     const title = ref('');
     const description = ref('');
     const rawContent = ref('');
     const user = ref({});
+
+    this.blogId = this.$route.params.blogId
+
+    const getBlogDetail = async() => {
+      const res = await blog.getDetail({ blogId: this.blogId})
+      this.title = res.data.title
+      this.rawContent = res.data.content
+      this.createdAt = res.data.createdAt
+      this.user = res.data.user
+    }
+
+    // 生成 网页内容 HTML
+    renderArticleDom = computed(() => {
+      return marked(this.rawContent)
+    });
 */
 
     return {};
@@ -25,19 +41,21 @@ export default defineComponent({
       <>
         <section class={cssDetail.userBlog}>
           <img src="https://cn.gravatar.com/avatar/1?s=128&d=identicon"
-               alt=""
+               alt="user.name"
+               title="user.username"
                class={cssDetail.avatar}/>
           <h3 class={cssDetail.title}>前端异步大揭秘</h3>
           <p class={cssDetail.user}>
-            <router-link to="/user"
+            <router-link to="/user/${user.id}"
                          class={cssDetail.userPage}>
               若愚
             </router-link>
-            发布于3天前
+            发布于 3天前
           </p>
         </section>
 
         <section class={classNames([cssDetail.article, 'article'])}>
+          {/* renderArticleDom() */}
           <h1 id="css-网格布局学习指南">CSS 网格布局学习指南</h1>
           <p>文章正文,省略一万字...</p>
         </section>
