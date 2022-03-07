82e4cf3d84baf98fc25c8b43eeea08962e13f88b
xmasuhai <xmasuhai@163.com>
2022-03-07 09:58:56 +0800

    ♻️ 重构markdown生成函数

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,9 +1,16 @@
 import {marked} from 'marked';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
-import 'highlight.js/styles/github.css';
+import typescript from 'highlight.js/lib/languages/typescript';
+import css from 'highlight.js/lib/languages/css';
+import scss from 'highlight.js/lib/languages/scss';
+import 'highlight.js';
+import "highlight.js/styles/atom-one-dark.css";
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('scss', scss);
 
 export default function markdown(rawContent: string) {
   marked.setOptions({
@@ -14,6 +21,7 @@ export default function markdown(rawContent: string) {
     sanitize: false,
     smartLists: true,
     smartypants: false,
+    langPrefix: 'hljs language-',
     highlight(code) {
       return hljs.highlightAuto(code).value;
     }
